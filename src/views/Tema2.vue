9d5cfa94fc06e457db7d91abe3b192db7eed84a2
Luis Perez <ljpm9401@gmail.com>
2022-04-25 20:07:38 -0500

    Ajustes videos

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -257,7 +257,7 @@
               img(src='@/assets/curso/tema2/img8.png').m-auto
         figure.mb-5
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/yqYEn9uhtwQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     separador.mb-5
     #t_2_2
     .titulo-segundo.color-secundario
