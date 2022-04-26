ed2d4d3bb3e9204a981013711e64a22734eb42d1
Luis Perez <ljpm9401@gmail.com>
2022-04-25 20:23:22 -0500

    Ajustes

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -5,12 +5,12 @@
 
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+        .tarjeta.h-100.d-flex.align-items-center
           figure
             img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Actividad didáctica
+          h2 Generalidades de la salud mental
         p.mb-4 Identificar conceptos específicos de la salud mental.
         .tarjeta.tarjeta--azul.p-3
           .row.justify-content-around.align-items-center            
